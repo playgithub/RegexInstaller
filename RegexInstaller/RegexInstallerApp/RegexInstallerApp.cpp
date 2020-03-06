a8224340f4bb4f6008f428c8118bb49da9fda240
a <a@b.com>
2020-03-07 00:10:01 +0800

    support both GUI and console mode in one application with --console-mode it'll run in console mode, and --install-file-path="path\to\install\file"  is needed.

--- RegexInstaller/RegexInstallerApp/RegexInstallerApp.cpp
+++ RegexInstaller/RegexInstallerApp/RegexInstallerApp.cpp
@@ -1,21 +1,77 @@
 #include "pch.h"
+#include <RegexInstaller/RegexInstaller.h>
 #include <UI/MainFrame.h>
 #include "RegexInstallerApp.h"
 
 IMPLEMENT_APP(RegexInstallerApp)
 
+RegexInstallerApp::RegexInstallerApp() :
+    _is_console_mode(false)
+{
+}
+
 bool RegexInstallerApp::OnInit()
 {
-    MainFrame * pMF = new MainFrame(nullptr, wxID_ANY, "Install Tool", wxDefaultPosition, wxSize(800, 600));
+    if (!wxApp::OnInit())
+    {
+        wxLogDebug(wxT("wxApp::OnInit failed"));
+        return false;
+    }
 
-    pMF->Center();
-    
-    pMF->Show();
+    if (!_is_console_mode)
+    {
+        MainFrame * pMF = new MainFrame(nullptr, wxID_ANY, "Install Tool", wxDefaultPosition, wxSize(800, 600));
+        pMF->Center();
+        pMF->Show();
+    }
 
     return true;
+}
+
+int RegexInstallerApp::OnRun()
+{
+    if (_is_console_mode)
+    {
+        try
+        {
+            RegexInstaller installer;
+            installer.Install(_install_file_path_in_console_mode.ToStdString());
+            return 0;
+        }
+        catch (std::exception & ex)
+        {
+            wxLogDebug("RegexInstaller::Install failed (%s)", ex.what());
+            return -1;
+        }
+    }
+    else
+    {
+        return wxApp::OnRun();
+    }
 }
 
 int RegexInstallerApp::OnExit()
 {
     return wxApp::OnExit();
+}
+
+void RegexInstallerApp::OnInitCmdLine(wxCmdLineParser & parser)
+{
+    parser.AddLongSwitch("console-mode", "run in console mode", wxCMD_LINE_VAL_NONE);
+    parser.AddLongOption("install-file-path", "install file path", wxCMD_LINE_VAL_STRING);
+}
+
+bool RegexInstallerApp::OnCmdLineParsed(wxCmdLineParser & parser)
+{
+    _is_console_mode = parser.FoundSwitch("console-mode");
+    if (_is_console_mode)
+    {
+        if (!parser.Found("install-file-path", &_install_file_path_in_console_mode))
+        {
+            wxLogDebug(L"no install-file-path");
+            return false;
+        }
+    }
+
+    return true;
 }
--- RegexInstaller/RegexInstallerApp/RegexInstallerApp.h
+++ RegexInstaller/RegexInstallerApp/RegexInstallerApp.h
@@ -3,8 +3,16 @@
 class RegexInstallerApp : public wxApp
 {
 public:
-    virtual bool OnInit();
-    virtual int OnExit();
+    RegexInstallerApp();
+public:
+    virtual bool OnInit() wxOVERRIDE;
+    virtual int OnRun() wxOVERRIDE;
+    virtual int OnExit() wxOVERRIDE;
+    virtual void OnInitCmdLine(wxCmdLineParser & parser) wxOVERRIDE;
+    virtual bool OnCmdLineParsed(wxCmdLineParser & parser) wxOVERRIDE;
+private:
+    bool _is_console_mode;
+    wxString _install_file_path_in_console_mode;
 };
 
 DECLARE_APP(RegexInstallerApp)
